d3cc480479f1b3e8051905e65f56b01aedac6902
awhr95 <awhr95@gmail.com>
2024-03-29 16:16:19 +0000

    Fix quiz typo

--- src/pages/quiz/Quiz.js
+++ src/pages/quiz/Quiz.js
@@ -25,70 +25,61 @@ const Quiz = () => {
     event.preventDefault();
     if (selectedLeague && selectedYear) {
       setQuizStarted(true);
-       notifySuccess("Quiz Starting!");
+      notifySuccess("Quiz Starting!");
       setTimeout(() => {
         navigate(`/quiz-body?leagueId=${selectedLeague}&year=${selectedYear}`);
       }, 5000);
     }
   };
-    }
-   
-};
 
-return (
-  <div>
-    <Header />
-    <form onSubmit={handleSubmit}>
-      <label>Select League:</label>
-      <select value={selectedLeague} onChange={handleLeagueChange}>
-        <option value="">Select League</option>
-        <option value="39">Premiere League</option>
-        <option value="1">World Cup</option>
-        <option value="2">Uefa Champions League</option>
-        <option value="45">FA Cup</option>
-        <option value="61">Ligue 1, France</option>
-        <option value="78">Bundesliga, Germany</option>
-        <option value="135">Serie A, Italy</option>
-        <option value="140">La Liga, Spain</option>
-        <option value="144">Pro League, Belgium</option>
-        <option value="120">Division 1, Denmark</option>
-        <option value="183">Scotish Premiere League</option>
-      </select>
+  return (
+    <div>
+      <Header />
+      <form onSubmit={handleSubmit}>
+        <label>Select League:</label>
+        <select value={selectedLeague} onChange={handleLeagueChange}>
+          <option value="">Select League</option>
+          <option value="39">Premiere League</option>
+          <option value="1">World Cup</option>
+          <option value="2">Uefa Champions League</option>
+          <option value="45">FA Cup</option>
+          <option value="61">Ligue 1, France</option>
+          <option value="78">Bundesliga, Germany</option>
+          <option value="135">Serie A, Italy</option>
+          <option value="140">La Liga, Spain</option>
+          <option value="144">Pro League, Belgium</option>
+          <option value="120">Division 1, Denmark</option>
+          <option value="183">Scotish Premiere League</option>
+        </select>
 
-      <label>Select Year:</label>
-      <select value={selectedYear} onChange={handleYearChange}>
-        <option value="">Select Year</option>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-        <option value="2014">2014</option>
-        <option value="2013">2013</option>
-        <option value="2012">2012</option>
-        <option value="2011">2011</option>
-        <option value="2010">2010</option>
-        {/* Add more year options as needed - maybe this should be user just inputs the year or it's randomised? */}
-      </select>
+        <label>Select Year:</label>
+        <select value={selectedYear} onChange={handleYearChange}>
+          <option value="">Select Year</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+          <option value="2011">2011</option>
+          <option value="2010">2010</option>
+          {/* Add more year options as needed - maybe this should be user just inputs the year or it's randomized? */}
+        </select>
 
-      <button type="submit">START</button>
-      
+        <button type="submit">START</button>
+      </form>
+
       {quizStarted && (
-        <QuizBody
-          selectedLeague={selectedLeague}
-          selectedYear={selectedYear}
-        />
+        <QuizBody selectedLeague={selectedLeague} selectedYear={selectedYear} />
       )}
-    </form>
-  </div>
-);
-}
-     
-
-
+    </div>
+  );
+};
 
 export default Quiz;
