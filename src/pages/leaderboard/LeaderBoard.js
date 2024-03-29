3c9c2d2517615c0c4b60cc39cf2d50126f00d768
awhr95 <awhr95@gmail.com>
2024-03-29 11:01:37 +0000

    Refactor sort

--- src/pages/leaderboard/LeaderBoard.js
+++ src/pages/leaderboard/LeaderBoard.js
@@ -1,33 +1,30 @@
 import Header from "../../components/header/Header";
 import Nav from "../../components/nav/Nav";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
-export default function leaderboard() {
+export default function Leaderboard() {
   const [leaderboard, setLeaderboard] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:8100/players");
-        console.log(response.data);
         setLeaderboard(response.data);
       } catch (error) {
         console.error(error.message);
       }
     };
     fetchData();
-  });
-
-  function sortLeaderboard(leaderboard) {
-    leaderboard.sort((a, b) => b.score - a.score);
-
-    return leaderboard;
-  }
-
-  const sortedLeaderboard = sortLeaderboard(leaderboard);
+  }, []);
 
   if (!leaderboard) {
     return <p>Loading...</p>;
   }
+
+  const sortedLeaderboard = leaderboard.sort((a, b) => b.score - a.score);
+
+  console.log(sortedLeaderboard);
 
   return (
     <>
